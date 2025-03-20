5c9cddb8c9224649a8920243574a538bd7c2ef35
Cupsz <yusuf.tif422@polban.ac.id>
2025-03-20 11:07:51 +0700

    folder CQRS & Saga

--- order-service/src/main/java/com/techie/microservice/order/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservice/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservice.order.service;
 
+import com.techie.microservice.order.client.InventoryClient;
 import com.techie.microservice.order.dto.OrderRequest;
 import com.techie.microservice.order.model.Order;
 import com.techie.microservice.order.repository.OrderRepository;
@@ -13,13 +14,22 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+
+        var isProduckInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProduckInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+
     }
 }
